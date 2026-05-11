deab329410194b96dc74caa2973bd4ba3ddfb835
Manon Colard <HE202545@students.ephec.be>
2026-05-11 17:26:24 +0200

    accessibilité front,back

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes";
+import  UserRoutes  from "./routes/userRoutes.js";
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoutes);
+app.use("/users", UserRoutes);
 app.get("/", (req, res) => {
   res.json({ message: "API is running 🚀" });
 });
