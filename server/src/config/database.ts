9fdfa56148c8b3e7a238fe084b59c8790fc2a1cb
Manon Colard <HE202545@students.ephec.be>
2026-05-11 15:21:37 +0200

    debug

---
+++ server/src/config/database.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { Sequelize } from "sequelize";
+
+const dbHost = process.env.DB_HOST;
+if (!dbHost) {
+  throw new Error("DB_HOST is not defined");
+}
+
+export const sequelize = new Sequelize(
+  process.env.DB_NAME as string,      // nom de la base
+  process.env.DB_USER as string,      // utilisateur
+  process.env.DB_PASSWORD as string,  // mot de passe
+  {
+    host: dbHost,        // ex: localhost
+    dialect: "mysql",
+    logging: false,
+  }
+);
+
+export default sequelize;
