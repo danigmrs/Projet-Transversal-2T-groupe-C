499de6a5ee37d4c384136d9ecdffc8bb669d0e2b
Manon Colard <HE202545@students.ephec.be>
2026-05-11 14:17:31 +0200

    middlewares

--- client/src/pages/Inscription.tsx
+++ client/src/pages/Inscription.tsx
@@ -51,7 +51,7 @@ export default function Inscription() {
   };
 
   const goToHome = () => {
-    navigate("/Home");
+    navigate("/");
   };
 
   return (
---
+++ server/src/middlewares/checkIdParam.ts
@@ -0,0 +1,17 @@
+import type { Request, Response, NextFunction } from "express";
+
+export const checkIdParam = (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+
+    //if (typeof id !== "string") {
+    //    return res.status(400).json({ error: "ID invalide" });
+    //}
+
+    const idNum = parseInt(id, 10);
+
+    if (isNaN(idNum) || idNum <= 0) {
+        return res.status(400).json({ error: "ID invalide" });
+    }
+
+    next();
+};
